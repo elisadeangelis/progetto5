cf402a7a3b7972de2ffc999c7523c26f89f93206
erssav <fsaveriomessina@gmail.com>
2024-05-21 11:39:57 +0200

    main museo

---
+++ db_museo/main_DB.py
@@ -0,0 +1,15 @@
+from funzioni_db import *
+from query import *
+
+
+
+DB_NAME = "museo"
+
+connection = create_server_connection("localhost","root","")
+execute_query(connection, f"drop database {DB_NAME}")
+create_database(connection, DB_NAME)
+connection = create_db_connection("localhost", "root","", DB_NAME) 
+
+
+execute_query(connection, create_table_artisti)
+execute_query(connection, create_table_artworks)
