cd3645ec1051600925521092540842d98beffc61
elisadeangelis <elisadeangelisss@gmail.com>
2024-05-21 09:58:03 +0200

    aggiunta route music

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("home.html")
+@app.route('/music')
+def music():
+    return render_template("music.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
-   
+
