f71beaa95fc10a6a68dd44e90afb8843e439786a
Simone <steamrail94@gmail.com>
2024-05-22 09:58:13 +0200

    inserimento percorsi app

--- app.py
+++ app.py
@@ -6,22 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("home.html")
-@app.route('/music')
-def music():
-    return render_template("music.html")
+@app.route('/artisti')
+def artisti():
+    return render_template("artisti.html")
 
-@app.route('/book')
-def book():
-    return render_template("book.html")
-
-@app.route('/book2')
-def book2():
-    return render_template("book2.html")
-
-
-@app.route('/music2')
-def music2():
-    return render_template("music2.html")
+@app.route('/opere')
+def opere():
+    return render_template("opere.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
